4d33a44c5919221063ac55a1051ccad448e5df8e
thinhvpg2410 <thinh.vpg@gmail.com>
2024-10-30 12:06:12 +0700

    [Add] dump data product, categories

---
+++ data.js
@@ -0,0 +1,173 @@
+export const product = [
+    {
+        id: 1,
+        title: 'lorem ipsum',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        category: 'beauty',
+        price: 77.99,
+        rating: 4.3,
+        stock: 100,
+        images:[
+            "https://picsum.photos/200",
+            "https://picsum.photos/200",
+            "https://picsum.photos/200",
+        ],
+        reviews: [
+            {
+                rating: 2,
+                comment: "Very unhappy with my purchase!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "John Doe",
+                reviewerEmail: "john.doe@x.dummyjson.com"
+            },
+            {
+                rating: 2,
+                comment: "Not as described!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "Nolan Gonzalez",
+                reviewerEmail: "nolan.gonzalez@x.dummyjson.com"
+            },
+            {
+                rating: 5,
+                comment: "Very satisfied!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "Scarlett Wright",
+                reviewerEmail: "scarlett.wright@x.dummyjson.com"
+            }
+        ],
+    },
+    {
+        id: 2,
+        title: 'lorem ipsum',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        category: 'beauty',
+        price: 77.99,
+        rating: 4.3,
+        stock: 100,
+        images:[
+            "https://picsum.photos/200",
+            "https://picsum.photos/200",
+            "https://picsum.photos/200",
+        ],
+        reviews: [
+            {
+                rating: 2,
+                comment: "Very unhappy with my purchase!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "John Doe",
+                reviewerEmail: "john.doe@x.dummyjson.com"
+            },
+            {
+                rating: 2,
+                comment: "Not as described!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "Nolan Gonzalez",
+                reviewerEmail: "nolan.gonzalez@x.dummyjson.com"
+            },
+            {
+                rating: 5,
+                comment: "Very satisfied!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "Scarlett Wright",
+                reviewerEmail: "scarlett.wright@x.dummyjson.com"
+            }
+        ],
+    },
+    {
+        id: 3,
+        title: 'lorem ipsum',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        category: 'beauty',
+        price: 77.99,
+        rating: 4.3,
+        stock: 100,
+        images:[
+            "https://picsum.photos/200",
+            "https://picsum.photos/200",
+            "https://picsum.photos/200",
+        ],
+        reviews: [
+            {
+                rating: 2,
+                comment: "Very unhappy with my purchase!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "John Doe",
+                reviewerEmail: "john.doe@x.dummyjson.com"
+            },
+            {
+                rating: 2,
+                comment: "Not as described!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "Nolan Gonzalez",
+                reviewerEmail: "nolan.gonzalez@x.dummyjson.com"
+            },
+            {
+                rating: 5,
+                comment: "Very satisfied!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "Scarlett Wright",
+                reviewerEmail: "scarlett.wright@x.dummyjson.com"
+            }
+        ],
+    },
+    {
+        id: 4,
+        title: 'lorem ipsum',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        category: 'beauty',
+        price: 77.99,
+        rating: 4.3,
+        stock: 100,
+        images:[
+            "https://picsum.photos/200",
+            "https://picsum.photos/200",
+            "https://picsum.photos/200",
+        ],
+        reviews: [
+            {
+                rating: 2,
+                comment: "Very unhappy with my purchase!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "John Doe",
+                reviewerEmail: "john.doe@x.dummyjson.com"
+            },
+            {
+                rating: 2,
+                comment: "Not as described!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "Nolan Gonzalez",
+                reviewerEmail: "nolan.gonzalez@x.dummyjson.com"
+            },
+            {
+                rating: 5,
+                comment: "Very satisfied!",
+                date: "2024-05-23T08:56:21.618Z",
+                reviewerName: "Scarlett Wright",
+                reviewerEmail: "scarlett.wright@x.dummyjson.com"
+            }
+        ],
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        title: 'Electronics',
+        image: 'https://picsum.photos/200',
+    },
+    {
+        id: 2,
+        title: 'Fashion',
+        image: 'https://picsum.photos/200',
+    },
+    {
+        id: 3,
+        title: 'Beauty',
+        image: 'https://picsum.photos/200',
+    },
+    {
+        id: 4,
+        title: 'Fresh Fruit',
+        image: 'https://picsum.photos/200',
+    }
+]
