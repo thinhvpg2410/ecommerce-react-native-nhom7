07444cd2e24b0b987d21780c80a1d05a094a54aa
Ho Tram <21087811.tram@student.iuh.edu.vn>
2024-11-01 00:28:45 +0700

    SignUp and SignIn

---
+++ component/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+export default function HomeScreen({navigation}){
+    return (
+
+        <View>
+            <Text>Welcom to OnlineShop</Text>
+        </View>
+    );
+
+}
