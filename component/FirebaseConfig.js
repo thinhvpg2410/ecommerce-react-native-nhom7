8d0353d5a6b34a590a9169afe50db4278581884e
Ho Tram <21087811.tram@student.iuh.edu.vn>
2024-11-22 08:24:03 +0700

    config

--- component/FirebaseConfig.js
+++ component/FirebaseConfig.js
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getDatabase, ref, get, child } from "firebase/database";
-import 'firebase/database';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase, ref, get, child } from 'firebase/database'; 
+import { getFirestore } from 'firebase/firestore'; 
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyCJgRHLxJPi_eXa8sRPa48j6RIpgxvwERo",
   authDomain: "ecommerce-1d0da.firebaseapp.com",
@@ -19,7 +15,7 @@ const firebaseConfig = {
   measurementId: "G-BF5QXG53JM"
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-const database = getDatabase(firebaseApp);
+export const auth = getAuth(firebaseApp);
+export const database = getDatabase(firebaseApp);
+export const db = getFirestore(firebaseApp);
